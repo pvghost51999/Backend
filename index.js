c01081ad55eb56c2f5d43c36a35d15772e28050d
Prashant Verma <prashant.verma@nagarro.com>
2023-12-12 15:37:18 +0530

    latest

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const bodyParser=require('body-parser')
 const app=express();
 
 
-// app.use(express.static(path.join(__dirname,"/build")));
+app.use(express.static(path.join(__dirname,"/build")));
 
 
-// app.get("*",(req,res)=>{
-// res.sendFile(path.join(__dirname,"/build/index.html"));
-// })
+app.get("*",(req,res)=>{
+res.sendFile(path.join(__dirname,"/build/index.html"));
+})
 
 app.use(cors());
 app.use(express.json());
