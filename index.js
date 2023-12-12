9846f3acd67fca5d0f2dfcb3bd2366cd2509790a
Prashant Verma <prashant.verma@nagarro.com>
2023-12-12 16:23:44 +0530

    new site commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname,"/build")));
 
 app.get("*",(req,res)=>{
 res.sendFile(path.join(__dirname,"/build/index.html"));
-})
+}) 
 
 app.use(cors());
 app.use(express.json());
